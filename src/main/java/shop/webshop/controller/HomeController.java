d7d74f08a785d550effb12d045ad3c2d9e7e46c2
deki84 <deki1984@hotmail.de>
2024-11-24 07:38:52 +0100

    Implemented routing and navigation for all pages; fixed logo navigation issues

--- src/main/java/shop/webshop/controller/HomeController.java
+++ src/main/java/shop/webshop/controller/HomeController.java
@@ -1,19 +1,48 @@
 package shop.webshop.controller;
 
-import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-   @GetMapping("/")
+    @GetMapping(value = {"/"})
     public String home() {
-      return "index";
-    }
-    @GetMapping("/index.html")
-    public String redirectToHome() {
-        return "redirect:/";
+        return "redirect:/index.html";
     }
 
+    @GetMapping(value = {"/index.html"})
+    public String redirectToHome() {
+        return "index";
+    }
+
+    @GetMapping(value = {"/shop"})
+    public String shop() {
+        return "shop";
+    }
+
+    @GetMapping(value = {"/product"})
+    public String product() {
+        return "product";
+    }
+
+    @GetMapping(value = {"/contact"})
+    public String contact() {
+        return "contact";
+    }
+
+    @GetMapping(value = {"/about"})
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping(value = {"/cart"})
+    public String cart() {
+        return "cart";
+    }
+
+    @GetMapping(value = {"/checkout"})
+    public String checkout() {
+        return "checkout";
+    }
 }
